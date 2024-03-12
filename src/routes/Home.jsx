5fdf8eba7a5f661637b732b9dcb51cb7248ba86a
Matheus <limarios18@outlook.com>
2024-03-11 21:06:47 -0300

    Criando e usando o hook useMemo

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,6 +10,7 @@ import { useContext } from "react";
 import { SomeContext } from "../components/HookUseContext";
 import HookUseRef from "../components/HookUseRef";
 import HookUseCallback from "../components/HookUseCallback";
+import HookUseMemo from "../components/HookUseMemo";
 
 
 const Home = () => {
@@ -26,6 +27,7 @@ const Home = () => {
       <hr />
       <HookUseRef />
       <HookUseCallback />
+      <HookUseMemo />
     </div>
   );
 }
