b6a5ccd1a50385fe0ec85b4b75cd8fdd3775c14a
Matheus <limarios18@outlook.com>
2024-03-11 21:49:28 -0300

    Criando a hook custom e utilizando. Finalizando os hooks do react

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -13,6 +13,7 @@ import HookUseCallback from "../components/HookUseCallback";
 import HookUseMemo from "../components/HookUseMemo";
 import HookUseLayoutEffect from "../components/HookUseLayoutEffect";
 import HookUseImperativeHandle from "../components/HookUseImperativeHandle";
+import HookCustom from "../components/HookCustom";
 
 
 const Home = () => {
@@ -32,6 +33,7 @@ const Home = () => {
       <HookUseMemo />
       <HookUseLayoutEffect />
       <HookUseImperativeHandle />
+      <HookCustom />
     </div>
   );
 }
